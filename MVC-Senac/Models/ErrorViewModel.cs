1c301fd6e966d527c77c1527f30687c4bb68dfdf
Taylor-Lima <vinitei88@gmail.com>
2024-07-30 21:45:03 -0300

    Adicionar arquivos de projeto.

---
+++ MVC-Senac/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Senac.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
